6e7fc0f06a1a7b9bf13a9efac70c6fcd126c7e2b
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-05-20 13:16:36 -0400

    Excludeing mobile devices for simplicity

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,42 @@
 <template>
-  <router-view />
+  <div>
+    <div class="app flex flex-column">
+      <Navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+  </div>
+
 </template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+  components: {
+    Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -11,6 +47,22 @@
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
   background-color: #141625;
+}
+
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+
+  @media(min-width: 900px) {
+    flex-direction: row !important;
+
+  }
+
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
 }
 
 button,
@@ -80,6 +132,7 @@ button,
     border-radius: 50%;
     margin-right: 8px;
   }
+
   font-size: 12px;
   margin-right: 30px;
   align-items: center;
@@ -91,6 +144,7 @@ button,
   &::before {
     background-color: #33d69f;
   }
+
   color: #33d69f;
   background-color: rgba(51, 214, 160, 0.1);
 }
@@ -99,6 +153,7 @@ button,
   &::before {
     background-color: #ff8f00;
   }
+
   color: #ff8f00;
   background-color: rgba(255, 145, 0, 0.1);
 }
@@ -107,6 +162,7 @@ button,
   &::before {
     background-color: #dfe3fa;
   }
+
   color: #dfe3fa;
   background-color: rgba(223, 227, 250, 0.1);
 }
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,15 @@
 <template>
     <header class="flex">
         <div class="branding flex">
-            <img src="@/assets/file-invoice-dollar-solid.png" alt=""/>
+            <img src="@/assets/file-invoice-dollar-solid.png" alt="" />
         </div>
     </header>
 </template>
 
 <script>
-    export default {
-        name: "navigation",
-    }
+export default {
+    name: "navigation",
+}
 </script>
 
 <style lang="scss" scoped>
@@ -17,22 +17,25 @@ header {
     z-index: 99;
     flex-direction: row;
     background-color: #1e2139;
-    @media(min-width: 900px){
+
+    @media(min-width: 900px) {
         min-height: 100%;
         min-width: 90px;
         flex-direction: column;
         border-radius: 0 20px 20px 0;
     }
+
     .branding {
         border-radius: 0 20px 20px 0;
         background-color: #7c5dfa;
         justify-content: center;
         padding: 24px;
-        @media(min-width: 900px){
+
+        @media(min-width: 900px) {
             width: 100%;
         }
 
-        img{
+        img {
             width: auto;
             height: 30px;
         }
