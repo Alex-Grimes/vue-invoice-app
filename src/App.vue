0c612653d499860703a3adcd951c18474c424ace
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-05-24 18:02:30 -0400

    big commit

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,6 @@
 <template>
-  <div>
-    <div v-if="!mobile" class="app flex flex-column">
-      <Navigation />
-
-      <div class="app-content flex flex-column">
-        <transition name="invoice">
-          
-          <InvoiceModal v-if="invoiceModal"/>
-        </transition>
-        <router-view />
-      </div>
-    </div>
-    <div v-else class="mobile-message flex flex-column">
-      <h2>Sorry, this app is not suppourted on Mobile Devices</h2>
-      <p>To use this app, please use a Computer or Tablet</p>
-    </div>
-  </div>
+  <router-view />
 </template>
-
-<script>
-import { mapState } from 'vuex';
-import Navigation from './components/Navigation.vue';
-import InvoiceModal from './components/InvoiceModal.vue';
-export default {
-  data() {
-    return {
-      mobile: null,
-    };
-  },
-  components: {
-    Navigation,
-    InvoiceModal
-},
-  created() {
-    this.checkScreen();
-    window.addEventListener("resize", this.checkScreen);
-  },
-  methods: {
-    checkScreen() {
-      const windowWidth = window.innerWidth;
-      if (windowWidth <= 750) {
-        this.mobile = true;
-        return;
-      }
-      this.mobile = false;
-    },
-  },
-  computed: {
-    ...mapState(['invoiceModal'])
-  }
-};
-</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -60,51 +10,8 @@ export default {
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  //background-color: #141625;
-}
-
-.app {
   background-color: #141625;
-  min-height: 100vh;
-
-  @media(min-width: 900px) {
-    flex-direction: row !important;
-
-  }
-
-  .app-content {
-    padding: 0 20px;
-    flex: 1;
-    position: relative;
-  }
 }
-
-.mobile-message {
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #141625;
-  color: #fff;
-  
-  p{
-    margin-top: 16px;
-  }
-
-}
-
-//animatiion for Invoice modal
-
-.invoice-enter-active,
-.invoice-leave-active {
-  transition: 0.8s ease all;
-}
-
-.invoice-enter-from,
-.invoice-leave-to {
-  transform: translateX(-700px);
-}
-
 
 button,
 .button {
@@ -173,7 +80,6 @@ button,
     border-radius: 50%;
     margin-right: 8px;
   }
-
   font-size: 12px;
   margin-right: 30px;
   align-items: center;
@@ -185,7 +91,6 @@ button,
   &::before {
     background-color: #33d69f;
   }
-
   color: #33d69f;
   background-color: rgba(51, 214, 160, 0.1);
 }
@@ -194,7 +99,6 @@ button,
   &::before {
     background-color: #ff8f00;
   }
-
   color: #ff8f00;
   background-color: rgba(255, 145, 0, 0.1);
 }
@@ -203,7 +107,6 @@ button,
   &::before {
     background-color: #dfe3fa;
   }
-
   color: #dfe3fa;
   background-color: rgba(223, 227, 250, 0.1);
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    invoiceModal: null,
   },
   mutations: {
-    TOGGLE_INVOICE(state) {
-      state.invoiceModal = !state.invoiceModal;
-    }
   },
   actions: {
   },
